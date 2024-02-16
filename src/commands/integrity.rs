2de78b72f0a17a420cdf68a38742cb0f91d84bcf
Idra <percy.baker.0@gmail.com>
2024-02-16 23:12:53 +0000

    Remove debug + remove test-admin

--- src/commands/integrity.rs
+++ src/commands/integrity.rs
@@ -5,7 +5,6 @@ use crate::metadata::MetaData;
 pub fn integrity(metadata: &MetaData) {
     for server in metadata.get_servers() {
         let path = metadata.get_server_directory(&server) + "/plugins/";
-        println!("{}", path);
         for plugin in fs::read_dir(path).expect("Failed to read path") {
             let plugin = plugin.expect("Failed to get direntry");
             if plugin.metadata().expect("Failed to get file metadata").is_symlink() {
