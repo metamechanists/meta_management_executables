8c4c12c544525e4b487012206bf8d57db63924a2
Idra <percy.baker.0@gmail.com>
2024-02-16 23:11:49 +0000

    .

--- src/commands/integrity.rs
+++ src/commands/integrity.rs
@@ -4,7 +4,8 @@ use crate::metadata::MetaData;
 
 pub fn integrity(metadata: &MetaData) {
     for server in metadata.get_servers() {
-        let path = metadata.get_server_directory(&server);
+        let path = metadata.get_server_directory(&server) + "/plugins/";
+        println!("{}", path);
         for plugin in fs::read_dir(path).expect("Failed to read path") {
             let plugin = plugin.expect("Failed to get direntry");
             if plugin.metadata().expect("Failed to get file metadata").is_symlink() {
