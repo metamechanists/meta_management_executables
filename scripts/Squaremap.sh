a1b315f8e620b1d432c0e6e9b8ab34888b7ba4e9
Idra <percy.baker.0@gmail.com>
2024-02-22 16:00:23 +0000

    Add squaremap script

---
+++ scripts/Squaremap.sh
@@ -0,0 +1,2 @@
+url=`curl -s https://api.github.com/repos/jpenilla/squaremap/releases/latest | jq -r ".assets" | jq -c '[ .[] | select( .name | contains("paper")) ]' | jq -r '.[0].browser_download_url'`
+wget $url -nv -O $1/Squaremap.jar
