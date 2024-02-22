37b17d767b3f2dc435a44e415eb2f2fe9e45a471
Idra <percy.baker.0@gmail.com>
2024-02-22 18:44:56 +0000

    Fix SquaremapGP script

--- scripts/SquaremapGriefPrevention.sh
+++ scripts/SquaremapGriefPrevention.sh
@@ -1,5 +1,8 @@
+mkdir artifacts
+cd artifacts
 `wget -nv https://nightly.link/jpenilla/squaremap-addons/workflows/build/master/artifacts.zip`
 unzip artifacts.zip
+cd ..
 mv artifacts/squaremap-griefprevention-1.0.0-SNAPSHOT.jar $1/SquaremapGriefprevention.jar
 rm artifacts -R
 rm artifacts.zip
