48b15e152e6f943cf7cd6722df33a8d9f85f363e
Idra <percy.baker.0@gmail.com>
2024-02-22 18:45:35 +0000

    Fix typo

--- scripts/SquaremapGriefPrevention.sh
+++ scripts/SquaremapGriefPrevention.sh
@@ -3,6 +3,6 @@ cd artifacts
 `wget -nv https://nightly.link/jpenilla/squaremap-addons/workflows/build/master/artifacts.zip`
 unzip artifacts.zip
 cd ..
-mv artifacts/squaremap-griefprevention-1.0.0-SNAPSHOT.jar $1/SquaremapGriefprevention.jar
+mv artifacts/squaremap-griefprevention-1.0.0-SNAPSHOT.jar $1/SquaremapGriefPrevention.jar
 rm artifacts -R
 rm artifacts.zip
